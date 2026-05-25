2b06f6f5b1160d3d43de1f0ddf2f08f4eeebcdb1
Dimple043-tech <dimplesaxenax@gmail.com>
2026-05-25 21:26:30 +0530

    Added Sorting Algorithm

---
+++ Sorting_Algorithm/02_Optimization_Sorting.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using  namespace std ;
+void bubbleSort(int arr[],int n ){ // O(n^2)
+    
+    for(int i=0; i<n-1; i++){
+        bool isSwipe = false;
+        for(int j=0; j<n-(i+1); j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                isSwipe = true;
+            }
+        }
+        if(!isSwipe){ // array is already is sorted
+            return ;
+        }
+    }
+}
+
+
+void PrintArr(int arr[],int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+    
+}
+
+
+
+ int main(){
+    int arr[] = {4,1,5,2,3};
+    int n = 5 ;
+    bubbleSort(arr,n );
+     PrintArr(arr, n);
+    return 0 ;
+}
